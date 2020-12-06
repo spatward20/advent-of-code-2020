fa9256b0d2fd58185090e3ba515bbd1d1967e11a
Shivangi Patwardhan <spatward@uwaterloo.ca>
2020-12-06 18:58:17 -0500

    Day 1 and 2

---
+++ src/Day3_TogobbonTrajectory/TobogganTrajectory.java
@@ -0,0 +1,9 @@
+package Day3_TogobbonTrajectory;
+
+/**
+ * Created by shivangipatwardhan on 12/6/20.
+ */
+public class TobogganTrajectory {
+    //    Toboggan Trajectory
+    // reset x keep y the same, until y = maxLength to replicate the idea of forever continuing hill side.
+}
